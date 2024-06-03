dcd74c64fda1f3ef0bd2c7073600c197ddd0aefe
Aditya <adityathebest2019@gmail.com>
2024-06-03 19:54:08 +0530

    updated readme and deleted unused deps

--- src/utils/Navlink.tsx
+++ src/utils/Navlink.tsx
@@ -1,7 +1,6 @@
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import { useInView } from "react-intersection-observer";
 
 type Props = {
     children: ReactNode;
@@ -9,13 +8,11 @@ type Props = {
 } & LinkProps;
 const Navlink = ({ children, className, ...props }: Props) => {
     const router = useRouter();
-    const [ref, isInView] = useInView();
     const isActive =
         router.asPath.split("#").pop() ===
-            props.href.toString().split("#").pop() || isInView;
+        props.href.toString().split("#").pop();
     return (
         <Link
-            ref={ref}
             className={`${isActive ? "text-accent " : ""}${className ?? ""}`}
             {...props}
         >
